3151574f53c1535cfb0a3915f338effed1cdc652
Andrey Cheltyomov <master.haunt@gmail.com>
2024-08-26 18:08:47 +0700

    homework 14-2 functionality done

--- src/category.py
+++ src/category.py
@@ -10,6 +10,19 @@ class Category:
     def __init__(self, name, description, products=None):
         self.name = name
         self.description = description
-        self.products = products if products else []
+        self.__products = products if products else []
         Category.category_count += 1
-        Category.product_count = len(self.products)
+        Category.product_count = len(self.__products)
+
+    def add_product(self, product):
+        self.__products.append(product)
+
+    @property
+    def products(self):
+        products_list = []
+        for i in range(len(self.__products)):
+            products_list.append(
+                f"{self.__products[i].name}, "
+                f"{self.__products[i].price} руб. "
+                f"Остаток: {self.__products[i].quantity} шт.")
+        return products_list
--- src/product.py
+++ src/product.py
@@ -5,9 +5,38 @@ class Product:
     description: str
     price: float
     quantity: int
+    products_list = []
 
     def __init__(self, name, description, price, quantity):
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
+        Product.products_list.append({"name": name, "description": description, "price": price, "quantity": quantity})
+
+    @classmethod
+    def new_product(cls, product_properties: dict):
+        for product in cls.products_list:
+            if product["name"] == product_properties["name"]:
+                product_properties["quantity"] += product["quantity"]
+                if product["price"] < product_properties["price"]:
+                    product["price"] = product_properties["price"]
+                else:
+                    product_properties["price"] = product["price"]
+
+        return cls(product_properties["name"], product_properties["description"], product_properties["price"],
+                   product_properties["quantity"])
+
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, new_price: float):
+        if new_price <= 0.0:
+            print("Цена не должна быть нулевая или отрицательная")
+        else:
+            if new_price < self.__price:
+                if input("Введите символ 'y' для подтверждения меньшей цены: ") == "y":
+                    self.__price = new_price
+        return self.__price
