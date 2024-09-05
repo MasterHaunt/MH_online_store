3527fc890005e5a3da6b34e0df453826cea7c709
Andrey Cheltyomov <master.haunt@gmail.com>
2024-09-05 19:33:40 +0700

    homework 16-1 tests done

--- tests/test_category.py
+++ tests/test_category.py
@@ -3,7 +3,7 @@ import pytest
 
 def test_category_init(category_1):
     """Тестирование создания объекта класса 'Category', у которого атрибут 'products' содержит список из
-    трёх объектов класса 'Product'"""
+    трёх объектов класса 'Product' """
     assert category_1.name == "Category 1"
     assert category_1.description == "First category"
     assert category_1.product_count == 3
@@ -11,18 +11,26 @@ def test_category_init(category_1):
 
 
 def test_category_add_product(category_1, product_2):
-    """Тестирование метода добавления объекта класса "Продукт" в объект класса "Категория" """
+    """Тестирование метода добавления объекта класса 'Product' в объект класса 'Category' """
     category_1.add_product(product_2)
     assert category_1.product_count == 4
 
 
+def test_category_add_not_product(category_1):
+    """Тестирование выброса ошибки <TypeError> при попытке добавления объекта не из класса 'Product'
+     в объект класса 'Category' """
+    with pytest.raises(TypeError):
+        category_1.add_product("Not product")
+    assert category_1.product_count == 3
+
+
 def test_category_str(category_1):
-    """Тестирование строкового представления объекта класса 'Category'"""
+    """ Тестирование строкового представления объекта класса 'Category' """
     assert str(category_1) == "Category 1, количество продуктов: 410 шт."
 
 
 def test_iterator(category_iterator):
-    """Тестирование итератора объекта класса 'Category'"""
+    """ Тестирование итератора объекта класса 'Category' """
     iter(category_iterator)
     assert category_iterator.index == 0
     assert str(next(category_iterator)) == "Product_1-1, 131.25 руб. Остаток: 100 шт."
